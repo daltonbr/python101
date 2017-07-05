bddcae6c9ff70c0f2cc30f1b1eb223afbf747fe1
Dalton Lima <daltonvarussa@gmail.com>
2017-07-04 22:01:43 -0300

    last commit in the course

--- PluralSightCourse/webapp/main.py
+++ PluralSightCourse/webapp/main.py
@@ -1,4 +1,16 @@
-from hs_student import *
+#from hs_student import *
 
-james = HighSchoolStudent("james")
-print(james.get_name_capitalize())
+#james = HighSchoolStudent("james")
+
+#print(james.get_name_capitalize())
+
+from student import *
+
+
+typed_id = input("Enter a student id: ")
+typed_name = input("Enter a student name: ")
+typed_last_name = input("Enter a student last name: ")
+
+new_student = Student(typed_name, typed_last_name, typed_id)
+
+print(new_student)
